b29be1b76cbc0f78f53611428afe4a173b3158c3
chauhan.prashant <chauhan.prashant@YI1008475DT.Yash.com>
2019-03-12 14:58:01 +0530

    1

---
+++ Mockito-demo/src/test/java/com/example/demo/SomeBusinessStubTest.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SomeBusinessStubTest {
+	@Test
+	public void testFindTheGreatestFromAllData() {
+		SomeBusinessImpl businessImpl = new SomeBusinessImpl(new DataServiceStub());
+		int result = businessImpl.findTheGreatestFromAllData();
+		assertEquals(24, result);
+
+	}
+
+}
+
+class DataServiceStub implements DataService {
+	@Override
+	public int[] retrieveAllData() {
+		return new int[] { 24, 6, 15 };
+	}
+}
